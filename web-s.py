76e8ea4eaaa707092a905fb83c4c651e2397a93b
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-10-27 23:02:14 -0300

    refact: modularizando o codigo em funcoes

--- web-s.py
+++ web-s.py
@@ -1,44 +1,59 @@
-import time
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common import by
 from bs4 import BeautifulSoup
 import json
+import time
 
-# 1. Pegar conteúdo HTML a partir da URL
-url = 'https://www.exercitodoacoes.org.br/doacoes/pontos-de-coleta/'
-option = Options()
-option.headless = True
-driver = webdriver.Chrome(executable_path='./chromedriver', options=option)
 
-driver.get(url)
-time.sleep(5)
+def create_driver():
+    option = Options()
+    option.headless = True
+    return webdriver.Chrome(executable_path='./chromedriver', options=option)
 
-elements = driver.find_elements(by=by.By.XPATH, value="//div[@class='pontocoleta_bloco']")
-html_content = [e.get_attribute('outerHTML') for e in elements]
-driver.quit()
 
-# 2. Parsear o conteúdo HTML - BeautifulSoup
-soup = [BeautifulSoup(div, 'html.parser') for div in html_content]
-adresses = []
-for s in soup:
-    spans = s.find_all('span')
-    adresses.append([span.contents for span in spans])
+def get_html_content(url, driver):
+    driver.get(url)
+    time.sleep(5)
+    elements = driver.find_elements(by=by.By.XPATH, value="//div[@class='pontocoleta_bloco']")
+    html = [e.get_attribute('outerHTML') for e in elements]
+    driver.quit()
+    return html
 
-# 3. Transformar dados em um dicionário de dados próprio
-address = {}
-result = []
-for ad in adresses:
-    address['ponto'] = str(ad[0][0])
-    address['cidade'] = str(ad[1][0])
-    address['local'] = str(ad[2][0])
-    address['endereco'] = str(ad[3][0])
-    address['horario'] = str(ad[4][0])
-    address['praca'] = str(ad[6][0])
-    result.append(address)
 
-# 4. Converter e salvar em um arquivo JSON
-with open('enderecos.json', 'w') as jp:
-    js = json.dumps(result, indent=4, ensure_ascii=False)
-    jp.write(js)
+def get_donation_points(html):
+    soup = [BeautifulSoup(div, 'html.parser') for div in html]
+    adresses = []
+    for s in soup:
+        spans = s.find_all('span')
+        adresses.append([span.contents for span in spans])
+    return adresses
+
+
+def transform_data(data):
+    address = {}
+    result = []
+    for ad in data:
+        address['ponto'] = str(ad[0][0])
+        address['cidade'] = str(ad[1][0])
+        address['local'] = str(ad[2][0])
+        address['endereco'] = str(ad[3][0])
+        address['horario'] = str(ad[4][0])
+        address['praca'] = str(ad[6][0])
+        result.append(address)
+    return result
+
+
+def save_json_file(data):
+    with open('enderecos.json', 'w') as jp:
+        js = json.dumps(data, indent=4, ensure_ascii=False)
+        jp.write(js)
+
+
+if __name__ == '__main__':
+    url_donations = 'https://www.exercitodoacoes.org.br/doacoes/pontos-de-coleta/'
+    driver = create_driver()
+    html_content = get_html_content(url_donations, driver)
+    adresses = get_donation_points(html_content)
+    result = transform_data(adresses)
+    save_json_file(result)
