b34e3261ed08593a81c1dff7cdb2de4cc8873b74
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-12-14 21:19:49 -0300

    chore: run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,7 @@
 export DB_HOST='localhost'
 export DB_USER='root'
-export DB_PASSWORD='alme200*'
+export DB_PASSWORD=''
 export DB_NAME='db_donations'
+export MAPS_API_KEY='your-api-key'
 
 python3 main.py
