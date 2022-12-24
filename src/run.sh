29d60ef865c6dee56fd2e0c354f6b13c962b6052
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-12-24 16:28:15 -0300

    chore: adicionando variavel de ambiente

--- src/run.sh
+++ src/run.sh
@@ -3,6 +3,7 @@ export DB_USER='root'
 export DB_PASSWORD='root'
 export DB_NAME='db_donations'
 export MAPS_API_KEY=''
+export ENVIRONMENT='local'
 export ADDRESS_API_URL_LOCAL='http://localhost:5000/api'
 export ADDRESS_API_URL_PRD=''
 
